c38bd2ad55c70462a90aca9ff53c9b2db687b3a6
Andrei Nistor <coder.tux@ceata.org>
2010-06-29 22:37:39 +0300

    added directory stack API

---
+++ dirstack.c
@@ -0,0 +1,13 @@
+#include "dirstack.h"
+#include <stdlib.h>
+
+void init(dir_stack_t *ds){
+}
+
+int push (dir_stack_t *ds, char *path){
+  return 0;
+}
+
+char* pop(dir_stack_t *ds){
+  return NULL;
+}
---
+++ dirstack.h
@@ -0,0 +1,15 @@
+#ifndef __DIRSTACK_H
+#define __DIRSTACK_H
+
+#define STACKSIZE 128
+
+typedef struct {
+  int size;
+  char *dir[STACKSIZE];
+} dir_stack_t;
+
+void init(dir_stack_t *ds);
+int push (dir_stack_t *ds, char *path);
+char* pop(dir_stack_t *ds);
+
+#endif 
