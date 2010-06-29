dde0eb4333ea21bd10cba5f56f1f4f174702d22c
Andrei Nistor <coder.tux@ceata.org>
2010-06-30 00:07:57 +0300

    added dirstack implementation

--- dirstack.c
+++ dirstack.c
@@ -1,13 +1,29 @@
 #include "dirstack.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void init(dir_stack_t *ds){
+  ds->size = 0;
 }
 
-int push (dir_stack_t *ds, char *path){
-  return 0;
+int push(dir_stack_t *ds, char *path){
+  if (ds->size == STACKSIZE) // stack is full
+    return -1;
+  
+  ds->dir[ds->size++] = strdup(path);
+  return ds->size;
 }
 
 char* pop(dir_stack_t *ds){
-  return NULL;
+  if (ds->size == 0) // stack is empty
+    return NULL;
+  
+  return ds->dir[--ds->size];
+}
+
+void print(dir_stack_t *ds){
+  int i;
+  for (i=0; i<ds->size; i++)
+    printf("%s\n", ds->dir[i]);
 }
--- dirstack.h
+++ dirstack.h
@@ -9,7 +9,7 @@ typedef struct {
 } dir_stack_t;
 
 void init(dir_stack_t *ds);
-int push (dir_stack_t *ds, char *path);
+int push(dir_stack_t *ds, char *path);
 char* pop(dir_stack_t *ds);
-
+void print(dir_stack_t *ds);
 #endif 
