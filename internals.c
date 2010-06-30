b40e94dc3d053c94e422b728b1b04af193fb8c41
Andrei Nistor <coder.tux@ceata.org>
2010-07-01 01:48:44 +0300

    more comments in internals.c

--- internals.c
+++ internals.c
@@ -64,12 +64,15 @@ int run_internal(simple_command_t *s)
     }
   }
   
+  /* show directories in the stack */
   if(!strcmp(verb_str, DIRS)) {
     if (!print(&ds)){
       printf("Stack is empty.\n");
       return 1;
     }
   }
+  
+  /* pop directory off the stack */
   if(!strcmp(verb_str, POPD)) {
     dir_str = pop(&ds);
     if(dir_str) {
@@ -79,6 +82,8 @@ int run_internal(simple_command_t *s)
       return 1;
     }
   }
+  
+  /* echo [params ...] just echoes the parameters back to the user */
   if(!strcmp(verb_str, ECHO)) {
     int argc, i;
     char **argv;
@@ -87,7 +92,6 @@ int run_internal(simple_command_t *s)
       printf("%s ", argv[i]);
     }
     printf("\n");
-    
   }
   
   return 0;
